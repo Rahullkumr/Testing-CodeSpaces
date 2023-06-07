807dc6562121cbb301ec454c403e786e7ff4dea4
Rahul Kumar <kumarrahul771996@gmail.com>
2023-06-07 13:51:18 +0000

    hello

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "Hello, World!"
+    
+@app.route("/namaste")
+def hello_world():
+    return "Namaste, Duniya!"
 
 if __name__ == "__main__":
     app.run(debug=True)
